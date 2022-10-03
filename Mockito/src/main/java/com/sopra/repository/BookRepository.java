b716b8686251b9fddac0911b68b380d5a8f5aabc
AMAN Aman <aman.aman@soprasteria.com>
2022-10-03 12:23:41 +0530

    first commit

---
+++ Mockito/src/main/java/com/sopra/repository/BookRepository.java
@@ -0,0 +1,15 @@
+package com.sopra.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sopra.entity.Book;
+
+
+public interface BookRepository extends CrudRepository<Book, Integer>{
+	
+	public List<Book> findByName(String name);
+
+}
---
+++ Mockito/src/test/java/com/sopra/MockitoApplicationTests.java
@@ -0,0 +1,12 @@
+/*
+ * package com.sopra;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class MockitoApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
